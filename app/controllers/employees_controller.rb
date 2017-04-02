cbb97973834c19e384a284631f405b8f3d776065
Lee Richmond <lrichmond1@bloomberg.net>
2017-04-02 17:21:22 -0400

    Add basic error handling example

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,12 +1,18 @@
 class EmployeesController < ApplicationController
   jsonapi resource: EmployeeResource
 
+  register_exception ActiveRecord::RecordNotFound,
+    status: 422,
+    message: ->(e) { "Whoops! No employee found :(" }
+
   def index
     render_jsonapi(Employee.all)
   end
 
   def show
     scope = jsonapi_scope(Employee.where(id: params[:id]))
-    render_jsonapi(scope.resolve.first, scope: false)
+    employee = scope.resolve.first
+    raise ActiveRecord::RecordNotFound unless employee
+    render_jsonapi(employee, scope: false)
   end
 end
--- spec/api/v1/employees/show_spec.rb
+++ spec/api/v1/employees/show_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe 'v1/employees#show', type: :request do
     get "/api/v1/employees/#{employee.id}"
     assert_payload(:employee, employee, json_item)
   end
+
+  context 'when querying an employee that does not exist' do
+    before do
+      JsonapiErrorable.enable!
+    end
+
+    let(:expected) do
+      {
+        'errors' => [
+          {
+            'code'   => 'unprocessable_entity',
+            'status' => '422',
+            'title'  => 'Error',
+            'detail' => 'Whoops! No employee found :(',
+            'meta'   => {}
+          }
+        ]
+      }
+    end
+
+    it 'renders relevant error payload' do
+      get "/api/v1/employees/1"
+      expect(json).to eq(expected)
+    end
+  end
 end
