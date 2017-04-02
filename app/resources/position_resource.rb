41dcd3ba19e96505f468320b7151a272c297ade6
Lee Richmond <lrichmond1@bloomberg.net>
2017-04-02 17:06:58 -0400

    FSP positions from employee

--- app/resources/position_resource.rb
+++ app/resources/position_resource.rb
@@ -1,6 +1,10 @@
 class PositionResource < ApplicationResource
   type :positions
 
+  allow_filter :title_prefix do |scope, value|
+    scope.where(["title LIKE ?", "#{value}%"])
+  end
+
   belongs_to :department,
     scope: -> { Department.all },
     foreign_key: :department_id,
--- spec/api/v1/employees/index_spec.rb
+++ spec/api/v1/employees/index_spec.rb
@@ -44,5 +44,31 @@ RSpec.describe 'v1/employees#index', type: :request do
         assert_payload(:department, department2, json_departments[1])
       end
     end
+
+    describe 'when filtering/sorting/paginating positions' do
+      def create_position(title)
+        create :position,
+          employee_id: employee.id,
+          title: title
+      end
+
+      let!(:employee)  { create(:employee) }
+      let!(:position1) { create_position('product wwner') }
+      let!(:position2) { create_position('devops engineer') }
+      let!(:position3) { create_position('developer (junior)') }
+      let!(:position4) { create_position('developer (senior)') }
+
+      it 'filters/sorts/paginates correctly' do
+        get '/api/v1/employees', params: {
+          include: 'positions',
+          sort: 'positions.title',
+          filter: { positions: { title_prefix: 'dev' } },
+          page: { positions: { size: 1, page: 2 } }
+        }
+        json_positions = json_includes('positions')
+        expect(json_positions.length).to eq(1)
+        assert_payload(:position, position3, json_positions[0])
+      end
+    end
   end
 end
