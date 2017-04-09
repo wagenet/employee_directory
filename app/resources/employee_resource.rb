91128698ec186c914c8090487e526939dd6fb62e
Lee Richmond <lrichmond1@bloomberg.net>
2017-04-09 14:15:36 -0400

    Add example customizing persistence

--- app/resources/employee_resource.rb
+++ app/resources/employee_resource.rb
@@ -6,4 +6,30 @@ class EmployeeResource < ApplicationResource
     scope: -> { Position.all },
     foreign_key: :employee_id,
     resource: PositionResource
+
+  def create(attributes)
+    employee = Employee.create(attributes)
+    log('Created', employee)
+    employee
+  end
+
+  def update(attributes)
+    employee = Employee.find(attributes[:id])
+    employee.update_attributes(attributes.except(:id))
+    log('Updated', employee)
+    employee
+  end
+
+  def destroy(id)
+    employee = Employee.find(id)
+    employee.destroy
+    log('Deleted', employee)
+    employee
+  end
+
+  private
+
+  def log(prefix, employee)
+    Rails.logger.info "#{prefix} #{employee.first_name} Employee via API"
+  end
 end
